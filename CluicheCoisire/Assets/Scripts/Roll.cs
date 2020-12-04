06f9a7487edb4ef17bd8a919d5a50750baae3d29
James Bryan <jamesbryangamess@gmail.com>
2020-12-04 13:43:16 +0000

    started rolling ball mini game

---
+++ CluicheCoisire/Assets/Scripts/Roll.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roll : MonoBehaviour
+{
+    public float speed = 10F;
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 acc = Input.acceleration;
+        rb.AddForce(acc.x * speed, 0, acc.y * speed);
+    }
+}
